effc45ba1386e0c3f6519a38d6fe63bd1b665b33
mayavathi <mayachandre27@gmail.com>
2021-06-24 08:19:38 -0400

    Reverse Number

---
+++ src/com/bridgelabz/logical_programming/ReverseNumber/ReverseNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.logical_programming.ReverseNumber;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	public static void main(String args[])
+	   {
+	      int num=0;
+	      int reversenum =0;
+	      System.out.println("Enter your number: ");
+	      Scanner in = new Scanner(System.in);
+	        num = in.nextInt();
+	       while( num != 0 )
+	      {
+	          reversenum = reversenum * 10;
+	          reversenum = reversenum + num%10;
+	          num = num/10;
+	      }
+
+	      System.out.println("Reverse of number is: "+reversenum);
+	   }
+	}
